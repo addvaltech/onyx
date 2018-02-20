3b6b66424c0082b5c5b3b5214be400e54c191ee4
soontoong <soontoong.pua@addvalue.com.sg>
2018-02-20 17:00:23 +0800

    Relese candidate for Fijowave

--- beaglebone/brm_ctl/BRM_AP.py
+++ beaglebone/brm_ctl/BRM_AP.py
@@ -7,6 +7,7 @@ import datetime
 import threading # timer thread
 import logging
 import itertools
+import sys
 requests.packages.urllib3.disable_warnings() 
 
 import Adafruit_BBIO.GPIO as GPIO
@@ -20,11 +21,12 @@ import log
 login_user = 'admin'
 password = 'm@nufacturing'
 url = 'https://192.168.1.1/auth/' 
+headers = {'content-type' : 'application/json'}
     
-logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-9s) %(message)s',)
+# creates a Logger instance for debugging 
+logging.basicConfig(filename='/var/lib/cloud9/myprojects/onyx/onyx.log',level=logging.DEBUG,
+                    format='[%(asctime)s] %(levelname)s: %(message)s',)
                     
-
 signalstrength = 0.1
 #   Declare latitude and longitude
 latitude = 0.0
@@ -254,10 +256,10 @@ Output: none
 def start_leds():
     setled = setLed()
     print 'Start LEDs sequence...'
-    for led in [setled.led0(),setled.led1(),setled.led2(),setled.led3(),setled.led4(),setled.led5()]:
-        led
+#    for led in [setled.led0,setled.led1,setled.led2,setled.led3,setled.led4,setled.led5]:
+#        led
 #        print led
-        time.sleep(0.2)
+#        time.sleep(0.2)
 
 
     setled.led0()
@@ -287,8 +289,8 @@ Output: Displays output LEDS to let user know the signal
 def signalStrength(s):
     global signalstrength
     #print datetime.datetime.now()
-    sig = signalstrength
-    #print sig
+    sig = float(signalstrength)
+    print sig
     
     if sig > 55.0:
         s.bar55()
@@ -365,7 +367,12 @@ class MyRestFul():
     def get(self, rest):
         req = self.s.get(url+rest,auth=(login_user, password), verify=False) 
         return req
-    
+    def post(self, rest, payload):
+        req = self.s.post(url+rest,auth=(login_user, password), verify=False, data=payload, headers={'Content-Type': 'application/json'})
+        return req
+    def put(self, rest, payload):
+        req = self.s.put(url+rest,auth=(login_user, password), verify=False, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+        return req        
     def logout(self):
 #        req = self.s.get(url+"v1/config/setting/serial_number",auth=(login_user, password), verify=False) 
         self.s.close()
@@ -379,14 +386,57 @@ Input:  channel
 # Define a threaded callback function to run in another thread when events are detected  
 def gpio_8_7_callback(channel):  
     #if GPIO.input('P8_7'):     # if port P8_7 == 1  
+    logging.info('Exit Antenna-pointing triggered.')
     if GPIO.event_detected("P8_7"):
         log.info("Rising edge detected on " + channel)
     else:                  # if port P8_7 != 1  
         log.info("Falling edge detected on " + channel)
     t1.stop()        
-    start_leds()
+    t0.start()
+
+    
+    if signalstrength < 0.0:
+        log.err('Signal not available!')
+#    else:
+#        start_leds()
+    
+
+    logging.debug('AT Cmd: AT_IPOINT=1')
+    at_data = {'command' : 'AT_IPOINT=1'} # Exit antenna-poiting At-cmd (AT_IPOINT)
+    at_data = json.dumps(at_data)
+    log.infog(at_data)
+    req = requests.post('https://192.168.1.1/auth/v1/at',auth=('admin','m@nufacturing'), verify=False, data=at_data,headers={'Content-Type': 'application/json'})
+    if req.status_code == 200:
+        d = json.loads(req.text)
+        at_resp = d['response']
+        log.infog('AT:' + at_resp)
+        logging.debug('AT Resp: ' + at_resp)
+    else:
+        log.err('AT command failure!')
+        print (req.text)  
+        logging.warning('AT command failure: '+ req.text)
+  
+    t0.stop()
+         
     t1.start()  
-        
+
+
+def connected_to_brm(url='https://192.168.1.1/auth/v1/device/firmware/status', timeout=3):
+    try:
+        req = requests.get(url, auth=('admin','m@nufacturing'), verify=False, timeout=timeout)
+        print req.text
+        # HTTP errors are not raised by default, this statement does that
+        req.raise_for_status()
+        return True
+    except requests.HTTPError as e:
+        print("Checking internet connection failed, status code {0}.".format(
+        e.response.status_code))
+        logging.warning('Checking internet connection failed.')
+    except requests.ConnectionError:
+        print("No internet connection available.")
+        logging.warning('BRM Websocket not reachable.')
+    return False
+    
 """
 Main
 """
@@ -399,16 +449,43 @@ def main():
     password = "m@nufacturing"
     
     url = 'https://192.168.1.1/auth/'
+    brm_err_cnt = 5
 
+    logging.info('ONYX main started.')
 
+    t0 = RepeatedTimer(2, start_leds)
+    t0.start()
+#   start_leds()   
+#    time.sleep(20)
+    
+    for i in range(0, 5, 1):
+        print("Waiting BRM connection...")
+        if connected_to_brm():
+            break
+
+    check_usim = True
+    set_ant_flag = True
     s = requests.Session()
     s.auth = (user, password)
+   
+    t0.stop()
+   
+    try:
+        # initial BRM websocket checking
+        response = s.get(url,verify=False) 
+        print response.status_code        
+    except requests.HTTPError as e:
+        print("Checking internet connection failed, status code {0}.".format(e.response.status_code))
+        logging.warning('No internet connection available.')
+    except requests.ConnectionError:
+        print("No internet connection available.")
+        logging.error('Initial BRM websocket checking error. \nExit Onyx main!')
+        sys.exit(1)
 
-    response = s.get(url,verify=False)
-    
-    print response.status_code
      
-    start_leds()    
+#    start_leds() 
+
+
 #    t0 = RepeatedTimer(1, start_leds,sb)
 #    t0.start()
 #    req = requests.get(url,auth=(user, password), verify=False)    
@@ -420,12 +497,16 @@ def main():
 #    print req.status_code
 
 
+
+
     log.info('== BRM INFORMATION ==')
     req = s.get(url+"v1/config/setting/serial_number",auth=('admin', 'm@nufacturing'), verify=False) 
     print 'Serial number :' + req.text
+    logging.info('Serial number :' + req.text)
     req = s.get(url+"v1/device/temp",auth=('admin', 'm@nufacturing'), verify=False) 
     data = json.loads(req.text)
     print 'BRM Temperature :' , data['brmTemp'], 'Celcius'
+    logging.info('Temperature :' + req.text)
     
 #    geocode = getGeoCode('tai seng mrt')
     
@@ -434,7 +515,7 @@ def main():
 #    print("Latitude:%s\nLongitude:%s\n"%(latitude, longitude))  
 
 
-    req = s.get(url+"v1/device/signalStrength",auth=('admin', 'm@nufacturing'), verify=False) 
+    req = s.get(url+"v1/device/signalstrength",auth=('admin', 'm@nufacturing'), verify=False) 
     if req.status_code == 200:
         data = json.loads(req.text)
         print 'Signal Strength :' , data, 'dBHz'
@@ -453,8 +534,9 @@ def main():
     r = rest.get('v1/device/id')
     print 'id.: ' + r.text 
 
-    r = rest.get('v1/device/firmware')
-    print 'firmware ver.: ' + r.text
+#    r = rest.get('v1/device/firmware')
+#    #r.text = r.text.encode('utf-8').strip()
+#    print 'firmware ver.: ' + r.text
 
     r = rest.get('v1/device/bist')
     print 'BIST: ' + r.text
@@ -462,9 +544,19 @@ def main():
     r = rest.get('v1/config')
     print 'configuration: ' + r.text
     log.info('== BRM INFORMATION ==')
+    logging.info(r.text)
         
     #rest.logout()
-
+    
+    # Get antenna pointing status (bypass antenna pointing)   
+    req = rest.get('v1/device/antenna_pointing')
+    if req.status_code == 200:
+        data = json.loads(req.text)
+        ant_pointing = data['enabled']
+        print ant_pointing
+    else:
+        log.err('antenna_pointing!')
+        print (req.text)
 
     
 #    time.sleep(5)    
@@ -486,11 +578,14 @@ def main():
 
     ledState = False
     
-######### test timer ##############
+######### timer ##############
     sb = signalBar()
     t1 = RepeatedTimer(1, signalStrength, sb)
-    logging.debug('starting timers...')
+    logging.debug('Signal strength timer started...')
     t1.start()
+    
+#   t0 = RepeatedTimer(1, start_leds)
+#    t0.start()
 #    t2.start()
 #    logging.debug('waiting before canceling %s', t2.getName())
 #    time.sleep(2)
@@ -507,43 +602,118 @@ def main():
     logging.debug('done')
 ########################################    
 
-    print("Getting Signal Strength\r\n")
+    print("Main loop...\r\n")
     while True:
 
-        #r = rest.get('v1/signalStrength')
-        #if req.status_code == 200:
-        #    data = json.loads(req.text)
-        #    print 'Signal Strength :' , data, 'dBHz'
-        #    signalstrength = data['signalStrength']
-        #else:
-        #    log.err('Signal strength request failed!')
-        #    #print (req.status_code)    
-        #    signalstrength = -1.0
+        try:
+            # TODO: write code...
+     
+            req = rest.get('v1/device/signalstrength')
+            if req.status_code == 200:
+                data = json.loads(req.text)
+    #            print data
+                if req.text == '{}':# or data == '':
+    #                print req.text
+                    signalstrength = -1.0
+                else:
+                    signalstrength = data['signalstrength']
+                    print (req.text)
+                logging.debug(req.text)
+            else:
+                log.err('Signal strength request failed!')
+                print (req.text)    
+                signalstrength = -1.0
+                logging.warn(req.text)
+    
+            # USIM Status
+            if check_usim:
+                req = rest.get('v1/device/usim/status')
+                if req.status_code == 200:
+                    data = json.loads(req.text)
+                    sim_status = data['status']
+                    log.infog('USIM Status: ' + sim_status)
+                    logging.debug(req.text)
+                    check_usim = False;
+                else:
+                    #if req.status_code == 403:
+                    data = json.loads(req.text)
+                    err_msg = data['errors']['message']
+                    log.err('USIM error: ' + err_msg)
+                    logging.err(req.text)
+                
+                
+            # Location Update    
+            req = rest.get('v1/location')
+            if req.status_code == 200:
+                data = json.loads(req.text)
+                #print data
+                gps_fix = data['fix']['fixdesc']
+                lon = data['lon']
+                lat = data['lat']
+                log.info('Location (GPS) : %s (Lon:%s Lat:%s)' % (gps_fix, lon, lat))
+                logging.info(data)
+    #            print '  Lon : %s Lat: %s' % (lon, lat)
+            else:
+                log.err('location request failed!')
+                print (req.text)
+                logging.err(req.text)
+                
             
-        r = rest.get('v1/location')
-        if req.status_code == 200:
-            data = json.loads(req.text)
-            print 'location :' , data
-            timestamp = data['timestamp']
-            lat = data['lat']
-            lon = data['lon']
+            if (gps_fix == '3d') and (set_ant_flag) and (ant_pointing):
+               
+                req = rest.get('v1/device/satellites_table')
+                if req.status_code == 200:
+                    data = json.loads(req.text)
+                    print data
+                    ele_max = max(data,key=lambda item:item['elevation'])
+                    sat_id = ele_max['id']
+                    sat_elevation = ele_max['elevation']
+                    sat_azimuth = ele_max['azimuth']
+                    log.infog('Select satellites id: #%s (Elevation:%s Azimuth:%s)' % (sat_id, sat_elevation, sat_azimuth))
+                    logging.info('Select satellites id: #%s (Elevation:%s Azimuth:%s)' % (sat_id, sat_elevation, sat_azimuth))
+                    #set_ant_flag = True # set current satellte id
+                else:
+                    log.err('location satellites request failed!')
+                    print (req.text)
+                    logging.err('location satellites request failed!')
+                
+                time.sleep(3)  
+            #if set_ant_flag:   ## Set current satellite          
+                data = {'satid':sat_id}            
+                print data
+                req = rest.put('v1/device/current_satellite', payload=data)      
+                if req.status_code == 200:
+                    d = json.loads(req.text)
+                    log.infog('Set current satellite id:' + str(sat_id))
+                    logging.info('Set current satellite id:' + str(sat_id))
+                   # connection = data['co']
+                else:
+                    log.err('command failure')
+                    print (req.text)       
+                    logging.err(req.text)
+                set_ant_flag = False
+            time.sleep(1)  
             
-        else:
-            log.err('GNSS location request failure! ' + str(req.status_code))
-            #print (req.status_code)    
-            signalstrength = -1.0
-  
-        r = rest.get('v1/satellites')
-        if req.status_code == 200:
-            data = json.loads(req.text)
-            print 'satellite :' , data
-        else:
-            log.err('GNSS receiver request failure! ' + str(req.status_code))
-            #print (req.status_code)    
-            signalstrength = -1.0
+        except requests.exceptions.Timeout as e:
+            # Maybe set up for a retry, or continue in a retry loop
+            print ("Error Timeout:",e)
+            logging.error("Error Timeout:",e)
+        except requests.exceptions.ConnectionError as e:
+            print ("Error Connecting:",e)      
+            logging.error("Error Connecting:",e)
+        except requests.exceptions.TooManyRedirects as e:
+            # Tell the user their URL was bad and try a different one
+            print ("Error TooManyRedirects:",e)    
+            logging.error("Error TooManyRedirects:",e)
+        except requests.exceptions.RequestException as e:
+            # catastrophic error. bail.
+            print ("Error RequestException:",e)             
+            print e
+            logging.error("Error RequestException:",e)
+            sys.exit(1)
             
-        time.sleep(1)
-        
+             
+
 if __name__== '__main__':
     main()
 
